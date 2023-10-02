d312451cfd31a9e8f5df2180e3961396ab28cd61
Luis Fuentes <lsfuentes2@gmail.com>
2023-10-01 22:06:26 -0300

    Update Artist.java
    Agregada las anotaciones de lombok para Getter y Setter

--- src/main/java/com/luisfuentes/tareaapi/model/Artist.java
+++ src/main/java/com/luisfuentes/tareaapi/model/Artist.java
@@ -3,9 +3,14 @@ package com.luisfuentes.tareaapi.model;
 import java.time.LocalDate;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "artists")
+
+@Getter
+@Setter
 public class Artist {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,35 +21,4 @@ public class Artist {
 	private Integer age;
 	
 	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public LocalDate getBirthdate() {
-		return birthdate;
-	}
-	public void setBirthdate(LocalDate birthdate) {
-		this.birthdate = birthdate;
-	}
-	public Integer getAge() {
-		return age;
-	}
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
 }
