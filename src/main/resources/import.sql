782d09a0a51f9d3e5286efee56c4dd99255a5864
Luis Fuentes <lsfuentes2@gmail.com>
2023-10-03 11:19:46 -0300

    Create import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+{
+    "artistName": "Shakira",
+    "firstName": "Shakira",
+    "lastName": "Mebarak",
+    "birthdate": "1977-02-02",
+    "age": "46"
+}
+{
+    "artistName": "Karol G",
+    "firstName": "Carolina",
+    "lastName": "Giraldo",
+    "birthdate": "1991-02-14",
+    "age": "32"
+}
